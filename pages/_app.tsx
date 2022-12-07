5df5b0cfd3867f48080fb7a97c1c564217c95a58
leejh <leejh@helixtech.co.kr>
2022-12-07 12:24:35 +0900

    develop > 파일 정리 및 mui install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {CssBaseline} from '@mui/material'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <CssBaseline />
+    </>
+  )
 }
 
 export default MyApp
